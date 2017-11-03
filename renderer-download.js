4cb670ac3fdabf4154fb63d70f3918c9ba1228d2
tranxuanthang <tranthang.yb@outlook.com>
2017-11-03 15:02:01 +0700

    Improving download feature
    Slow down the download progress's update to reduce CPU usage, added speed and remaining time calculation, detect broken/corrupt content files, fix title with japanese character can't build into cias (hopefully).

--- renderer-download.js
+++ renderer-download.js
@@ -3,6 +3,8 @@ const {app} = require('electron').remote;
 var ipcRenderer = require('electron').ipcRenderer;
 var fs = require('fs');
 var request = require('request');
+var progress = require('request-progress');
+var moment = require('moment');
 var sanitizefn = require("sanitize-filename");
 const path = require('path');
 var exec = require('child_process').exec;
@@ -26,7 +28,9 @@ var tk = 0x140;
 var tikteminhex = '00010004d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0d15ea5e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000526f6f742d434130303030303030332d585330303030303030630000000000000000000000000000000000000000000000000000000000000000000000000000feedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedfacefeedface010000cccccccccccccccccccccccccccccccc00000000000000000000000000aaaaaaaaaaaaaaaa00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010014000000ac000000140001001400000000000000280000000100000084000000840003000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000';
 var magicinhex = '00010004919ebe464ad0f552cd1b72e7884910cf55a9f02e50789641d896683dc005bd0aea87079d8ac284c675065f74c8bf37c88044409502a022980bb8ad48383f6d28a79de39626ccb2b22a0f19e41032f094b39ff0133146dec8f6c1a9d55cd28d9e1c47b3d11f4f5426c2c780135a2775d3ca679bc7e834f0e0fb58e68860a71330fc95791793c8fba935a7a6908f229dee2a0ca6b9b23b12d495a6fe19d0d72648216878605a66538dbf376899905d3445fc5c727a0e13e0e2c8971c9cfa6c60678875732a4e75523d2f562f12aabd1573bf06c94054aefa81a71417af9a4a066d0ffc5ad64bab28b1ff60661f4437d49e1e0d9412eb4bcacf4cfd6a3408847982000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000526f6f742d43413030303030303033000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000158533030303030303063000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000137a0894ad505bb6c67e2e5bdd6a3bec43d910c772e9cc290da58588b77dcc11680bb3e29f4eabbb26e98c2601985c041bb14378e689181aad770568e928a2b98167ee3e10d072beef1fa22fa2aa3e13f11e1836a92a4281ef70aaf4e462998221c6fbb9bdd017e6ac590494e9cea9859ceb2d2a4c1766f2c33912c58f14a803e36fccdcccdc13fd7ae77c7a78d997e6acc35557e0d3e9eb64b43c92f4c50d67a602deb391b06661cd32880bd64912af1cbcb7162a06f02565d3b0ece4fcecddae8a4934db8ee67f3017986221155d131c6c3f09ab1945c206ac70c942b36f49a1183bcd78b6e4b47c6c5cac0f8d62f897c6953dd12f28b70c5b7df751819a9834652625000100010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010003704138efbbbda16a987dd901326d1c9459484c88a2861b91a312587ae70ef6237ec50e1032dc39dde89a96a8e859d76a98a6e7e36a0cfe352ca893058234ff833fcb3b03811e9f0dc0d9a52f8045b4b2f9411b67a51c44b5ef8ce77bd6d56ba75734a1856de6d4bed6d3a242c7c8791b3422375e5c779abf072f7695efa0f75bcb83789fc30e3fe4cc8392207840638949c7f688565f649b74d63d8d58ffadda571e9554426b1318fc468983d4c8a5628b06b6fc5d507c13e7a18ac1511eb6d62ea5448f83501447a9afb3ecc2903c9dd52f922ac9acdbef58c6021848d96e208732d3d1d9d9ea440d91621c7a99db8843c59c1f2e2c7d9b577d512c166d6f7e1aad4a774a37447e78fe2021e14a95d112a068ada019f463c7a55685aabb6888b9246483d18b9c806f474918331782344a4b8531334b26303263d9d2eb4f4bb99602b352f6ae4046c69a5e7e8e4a18ef9bc0a2ded61310417012fd824cc116cfb7c4c1f7ec7177a17446cbde96f3edd88fcd052f0b888a45fdaf2b631354f40d16e5fa9c2c4eda98e798d15e6046dc5363f3096b2c607a9d8dd55b1502a6ac7d3cc8d8c575998e7d796910c804c495235057e91ecd2637c9c1845151ac6b9a0490ae3ec6f47740a0db0ba36d075956cee7354ea3e9a4f2720b26550c7d394324bc0cb7e9317d8a8661f42191ff10b08256ce3fd25b745e5194906b4d61cb4c2e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000526f6f7400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001434130303030303030330000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007be8ef6cb279c9e2eee121c6eaf44ff639f88f078b4b77ed9f9560b0358281b50e55ab721115a177703c7a30fe3ae9ef1c60bc1d974676b23a68cc04b198525bc968f11de2db50e4d9e7f071e562dae2092233e9d363f61dd7c19ff3a4a91e8f6553d471dd7b84b9f1b8ce7335f0f5540563a1eab83963e09be901011f99546361287020e9cc0dab487f140d6626a1836d27111f2068de4772149151cf69c61ba60ef9d949a0f71f5499f2d39ad28c7005348293c431ffbd33f6bca60dc7195ea2bcc56d200baf6d06d09c41db8de9c720154ca4832b69c08c69cd3b073a0063602f462d338061a5ea6c915cd5623579c3eb64ce44ef586d14baaa8834019b3eebeed3790001000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000';
 var finalTitleKey;
-
+var data;
+var tempfilename;
+var filename;
 app.showExitPrompt = false;
 
 function isDev() {
@@ -104,13 +108,20 @@ function hex2binArr(inputhex){
 	return a;
 }
 
-function makecia(makeciadir, rawdir , ciadir){
-	execute(makeciadir+' "'+rawdir+'" "'+ciadir+'"', function(output) {
-		console.log(makeciadir+' "'+rawdir+'" "'+ciadir+'"');
+function makecia(makeciadir, rawdir, tempciadir, ciadir){
+	execute(makeciadir+' "'+rawdir+'" "'+tempciadir+'"', function(output) {
+		console.log(makeciadir+' "'+rawdir+'" "'+tempciadir+'"');
 		console.log(output);
 		output = output.replace(/(?:\r\n|\r|\n)/g, '<br />');
-		$('#makecia').html(''+makeciadir+' "'+rawdir+'" "'+ciadir+'"'+'<br>'+output+'');
-		$('#content').append('<div class="mdl-color--white mdl-color-text--grey-800 mdl-cell mdl-cell--12-col listing">Everything finished.</div>');
+		$('#makecia').html(''+makeciadir+' "'+rawdir+'" "'+tempciadir+'"'+'<br>'+output+'');
+		if (fs.existsSync(tempciadir)) {
+			fs.renameSync(tempciadir,ciadir);
+			$('#content').append('<div class="mdl-color--white mdl-color-text--grey-800 mdl-cell mdl-cell--12-col listing">'
+			+'Renamed <span class="mdl-chip"><span class="mdl-chip__text">'+tempfilename+'</span></span> to <span class="mdl-chip"><span class="mdl-chip__text">'+filename+'</span></span>'
+			+'<br>Everything finished.</div>');
+		} else {
+			$('#content').append('<div class="mdl-color--white mdl-color-text--grey-800 mdl-cell mdl-cell--12-col listing">Too bad. Something\'s wrong ;-(.</div>');
+		}
 		app.showExitPrompt = false;
 	});
 }
@@ -212,7 +223,7 @@ function choiceHandle(file_url , targetPath, cID, ContentCount, dldir){
 							$('#info'+cID).hide();
 							$('#instruct'+cID).hide();
 							document.querySelector('#p'+cID).MaterialProgress.setProgress(100);
-							$('#done'+cID).html('Content #'+cID+' is succesfully downloaded by user.');
+							$('#done'+cID).html('Content #'+cID+' is successfully downloaded by user.');
 							checkFinished(cID,ContentCount);
 						} else {
 							$('#instruct'+cID).html('Content #'+cID+' which you\'ve manually downloaded is probably corrupted.');
@@ -227,7 +238,7 @@ function choiceHandle(file_url , targetPath, cID, ContentCount, dldir){
 		   }
 	});
 	req.on('error', function() {
-        $('#info'+cID).html('Content #'+cID+' is failed to download.<br><button id="dlanyway" class="mdl-button mdl-js-button mdl-button--primary">Redownload</button>');
+        $('#info'+cID).html('Content #'+cID+' cannot be downloaded. Internet connection failed.<br><button id="dlanyway" class="mdl-button mdl-js-button mdl-button--primary">Redownload</button>');
 		$('#info'+cID+' #dlanyway').on('click', function(){
 			if (fs.existsSync(targetPath)) {fs.unlinkSync(targetPath);}
 			choiceHandle(file_url , targetPath, cID, ContentCount, dldir);
@@ -240,41 +251,56 @@ function downloadFile(file_url , targetPath, cID, ContentCount){
 	if (fs.existsSync(targetPath)) {
 		fs.unlinkSync(targetPath);
 	}
-    // Save variable to know progress
-    let received_bytes = 0;
-    let total_bytes = 0;
-
-    let req = request({
+	var total_bytes;
+	progress(request({
         method: 'GET',
-        uri: file_url
-    });
-
-    //var out = fs.createWriteStream(targetPath);
-	let cIData = cID;
-    //req.pipe(out);
-	let downloadfile = fs.createWriteStream(targetPath, {'flags': 'w'});
-    req.on('response', function ( data ) {
+		uri: file_url,
+		timeout: 5000
+    }), {
+		throttle: 200,                    // Throttle the progress event to 2000ms, defaults to 1000ms 
+		delay: 200,                       // Only start to emit after 1000ms delay, defaults to 0ms 
+		// lengthHeader: 'x-transfer-length'  // Length header to use, defaults to content-length 
+	})
+	.on('response', function ( data ) {
         // Change the total bytes value to get progress later.
         total_bytes = parseInt(data.headers['content-length' ]);
-    });
-
-    req.on('data', function(chunk) {
-		downloadfile.write(chunk, encoding='binary');
-        // Update the received bytes
-		received_bytes += chunk.length;
-        showProgress(received_bytes, total_bytes, cID);
-		app.showExitPrompt = true;
-    });
-
-    req.on('end', function() {
-		downloadfile.end();
+    })
+	.on('progress', function (state) {
+		// The state is an object that looks like this: 
+		// { 
+		//     percent: 0.5,               // Overall percent (between 0 to 1) 
+		//     speed: 554732,              // The download speed in bytes/sec 
+		//     size: { 
+		//         total: 90044871,        // The total payload size in bytes 
+		//         transferred: 27610959   // The transferred payload size in bytes 
+		//     }, 
+		//     time: { 
+		//         elapsed: 36.235,        // The total elapsed seconds since the start (3 decimals) 
+		//         remaining: 81.403       // The remaining seconds to finish (3 decimals) 
+		//     } 
+		// } 
+		console.log('progress', state);
+		showProgress(state, cID);
+	})
+	.on('error', function (err) {
+		$('#info'+cID).html('Content #'+cID+' is failed to download.<br><button id="redownload" class="mdl-button mdl-js-button mdl-button--primary">Redownload</button>');
+		$('#info'+cID+' #redownload').on('click', function(){
+			if (fs.existsSync(targetPath)) {fs.unlinkSync(targetPath);}
+			choiceHandle(file_url , targetPath, cID, ContentCount, dldir);
+		});
+		$('#info'+cID).show();
+		app.showExitPrompt = false;
+	})
+	.on('end', function () {
 		var checkValid = validFileCheck(targetPath,total_bytes);
 		if (checkValid == true){
-			$('#done'+cIData).html('Content #'+cIData+' is succesfully downloaded.');
-			checkFinished(cIData,ContentCount);
+			$('#info'+cID).hide();
+			document.querySelector('#p'+cID).MaterialProgress.setProgress(100);
+			$('#done'+cID).html('Content #'+cID+' is successfully downloaded.');
+			checkFinished(cID,ContentCount);
 		} else {
-			$('#done'+cIData).html('');
-			$('#info'+cIData).html('Content #'+cIData+' is somehow corrupted when downloading.<br><button id="redownload" class="mdl-button mdl-js-button mdl-button--primary">Redownload</button>');
+			$('#done'+cID).html('');
+			$('#info'+cID).html('Content #'+cID+' is somehow corrupted while transferring.<br><button id="redownload" class="mdl-button mdl-js-button mdl-button--primary">Redownload</button>');
 			$('#info'+cID+' #redownload').on('click', function(){
 				if (fs.existsSync(targetPath)) {fs.unlinkSync(targetPath);}
 				choiceHandle(file_url , targetPath, cID, ContentCount, dldir);
@@ -282,25 +308,22 @@ function downloadFile(file_url , targetPath, cID, ContentCount){
 			$('#info'+cID).show();
 		}
 		app.showExitPrompt = false;
-	});
-	req.on('error', function() {
-        $('#info'+cIData).html('Content #'+cIData+' is failed to download.<br><button id="redownload" class="mdl-button mdl-js-button mdl-button--primary">Redownload</button>');
-		$('#info'+cID+' #redownload').on('click', function(){
-			if (fs.existsSync(targetPath)) {fs.unlinkSync(targetPath);}
-			choiceHandle(file_url , targetPath, cID, ContentCount, dldir);
-		});
-		$('#info'+cID).show();
-		app.showExitPrompt = false;
-    });
+	})
+	.pipe(fs.createWriteStream(targetPath));
 }
 
-function showProgress(received, total, cID){
-    var percentage = (received * 100) / total;
+function showProgress(state, cID){
+	var percentage = state.percent*100;
+	var received = state.size.transferred;
+	var total = state.size.total;
+	var speed = humanFileSize(state.speed,true);
+	var remainingTime = moment.duration(parseInt(state.time.remaining), 'seconds').humanize();
     //console.log(percentage + "% | " + received + " bytes out of " + total + " bytes.");
 	$('#info'+cID).html('');
-	$('#done'+cID).html(percentage.toFixed(2) + '% | ' + humanFileSize(received,false) + ' out of ' + humanFileSize(total,false));
+	$('#done'+cID).html(percentage.toFixed(2) + '% | ' + humanFileSize(received,true) + ' out of ' + humanFileSize(total,false)
+		+'<br>Speed: '+speed+'/s | Remaining time: '+ remainingTime
+	);
 	document.querySelector('#p'+cID).MaterialProgress.setProgress(percentage);
-	//$("#output").append('<div class="mdl-color--white mdl-color-text--grey-800 mdl-cell mdl-cell--12-col listing">'+cID+': '+percentage.toFixed(2) + '% | ' + humanFileSize(received,false) + ' out of ' + humanFileSize(total,false) + '.</div>');
 }
 
 var cIDtotal = [];
@@ -308,7 +331,9 @@ function checkFinished(cID, ContentCount){
 	cIDtotal.push(cID);
 	if(cIDtotal.length==ContentCount){
 		$('#content').append('<div id="makecia" class="mdl-color--white mdl-color-text--grey-800 mdl-cell mdl-cell--12-col listing">All tasks finished. Executing make_cdn_cia.exe. It will take long if the cia file is large...</div>');
-		makecia(makeciadir, dldir, path.join(basedir,'cias',filename));
+		tempCiaDir = path.join(basedir,'cias',tempfilename);
+		ciaDir = path.join(basedir,'cias',filename);
+		makecia(makeciadir, dldir, tempCiaDir, ciaDir);
 	}
 }
 
@@ -361,11 +386,6 @@ function processDownloadTitle(results,data) {
 	ContentCount = parseInt(hexContentCount,16);
 	$('#contentcount').html(ContentCount);
 	
-	
-	//console.log('tikdatainhex with hextobinarr:');
-	//console.log(hex2binArr(tikteminhex));
-	//tikdataArr = Array.prototype.slice.call(tikdata);
-	
 	/* Replace #1 */
 	tmdcut = tmdByteTypedArray.slice(476, 478);
 	tmdcutArr = Array.prototype.slice.call(tmdcut);
@@ -381,7 +401,7 @@ function processDownloadTitle(results,data) {
 	//console.log('encTitleKeyArr: '+encTitleKeyArr+ ' and '+encTitleKeyArr.length);
 	Array.prototype.splice.apply(tikdataArr, [447, encTitleKeyArr.length].concat(encTitleKeyArr));
 	
-	
+
 	/* Create cetk file by joining tikdata with magic */
 	var finalCetk = new Buffer(tikdataArr.concat(magic));
 	
@@ -429,6 +449,9 @@ function processDownloadTitle(results,data) {
 			if(os.platform()=='linux' || os.platform()=='darwin'){
 				fs.chmodSync(makeciadir,'744');
 			}
+
+			/* Create download tasks for each cID */
+
 			for(var i = 0; i < ContentCount; i++) {
 				//console.log('i='+i);
 				var cOffs = 0xB04+(0x30*i);
@@ -440,14 +463,12 @@ function processDownloadTitle(results,data) {
 			}
 		}
 	);
-
-	/* Create download tasks for each cID */
-	
-	
+	tempfilename = sanitizefn(data.titleID+'.cia');
 	filename = sanitizefn(data.name+' '+data.region+' ('+data.titleID+').cia');
 	//}
 }
-function processDownload(event, data){
+function processDownload(event, receivedData){
+	data = receivedData;
 	if(!data.titleID || !data.region ||!data.name){
 		$("#name").html('ERROR');
 		$("#region").html('ERROR');
